317ea28a8dfaa3919efd2388f60ac8f44ebbbb07
jordy <jordy.van.domselaar@outlook.com>
2018-01-25 13:52:25 +0100

    Move code to src/ dir.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-env GOOS=darwin GOARCH=amd64 go build -o dist/scaffoldeer_mac scaffoldeer.go
-env GOOS=linux GOARCH=amd64 go build  -o dist/scaffoldeer_linux scaffoldeer.go
-env GOOS=windows GOARCH=amd64 go build -o dist/scaffoldeer.exe scaffoldeer.go
+env GOOS=darwin GOARCH=amd64 go build -o dist/scaffoldeer_mac src/scaffoldeer.go
+env GOOS=linux GOARCH=amd64 go build  -o dist/scaffoldeer_linux src/scaffoldeer.go
+env GOOS=windows GOARCH=amd64 go build -o dist/scaffoldeer.exe src/scaffoldeer.go
--- src/scaffoldeer.go
+++ src/scaffoldeer.go
@@ -1,4 +1,4 @@
-package main
+package src
 
 import (
     "gopkg.in/urfave/cli.v1"

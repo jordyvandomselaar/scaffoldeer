951d9c5bb7b37245973dcef178dac824c6d76ea6
jordy <jordy.van.domselaar@outlook.com>
2018-01-25 08:43:35 +0100

    Code cleanup.

--- scaffoldeer.go
+++ scaffoldeer.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-    "bytes"
     "gopkg.in/urfave/cli.v1"
-    "io"
     "io/ioutil"
     "os"
     "path"
@@ -34,6 +32,12 @@ func main() {
     app.Run(os.Args)
 }
 
+type Stub struct {
+    FullPath, RelativePath, Name string
+    Content                      []byte
+}
+
+// Scaffold a template.
 func scaffold(c *cli.Context) error {
     fields := c.String("fields")
 
@@ -54,8 +58,20 @@ func scaffold(c *cli.Context) error {
 
     handleError(err)
 
-    parsedFields := strings.Split(fields, ",")
+    replacements := parseReplacements(fields)
+
+    stubs = parseStubs(stubs, replacements)
+
+    writeStubs(stubs)
+
+    return nil
+}
+
+// Parse argument string into map. --fields name:foo -> map[name] = "foo"
+func parseReplacements(fields string) map[string]string {
     replacements := make(map[string]string)
+
+    parsedFields := strings.Split(fields, ",")
 
     for _, value := range parsedFields {
         parsedFieldsSeparated := strings.Split(value, ":")
@@ -63,28 +79,48 @@ func scaffold(c *cli.Context) error {
         replacements[parsedFieldsSeparated[0]] = parsedFieldsSeparated[1]
     }
 
-    copyStubs(stubs, replacements)
-
-    return nil
+    return replacements
 }
 
-func copyStubs(stubs []Stub, replacements map[string]string) {
+// Copy stubs to another the current folder.
+func writeStubs(stubs []Stub) {
     for _, stub := range stubs {
-        stubNewName := parseReplacements(stub.Name, replacements)
-        stubNewRelativePath := parseReplacements(stub.RelativePath, replacements)
+        os.MkdirAll(stub.RelativePath, 0777)
 
-        stubNewName = strings.Replace(stubNewName, ".stub", "", -1)
-
-        os.MkdirAll(stubNewRelativePath, 0777)
-        copyFile(stub.FullPath, path.Join(stubNewRelativePath, stubNewName), replacements)
+        ioutil.WriteFile(
+            path.Join(stub.RelativePath, stub.Name),
+            stub.Content,
+            0777,
+        )
     }
 }
 
+// Parse stubs
+func parseStubs(stubs []Stub, replacements map[string]string) []Stub {
+    parsedStubs := []Stub{}
+
+    for _, stub := range stubs {
+        stubNewName := replacePlaceholders(stub.Name, replacements)
+        stubNewName = strings.Replace(stubNewName, ".stub", "", -1)
+        stubNewRelativePath := replacePlaceholders(stub.RelativePath, replacements)
+        stubNewContent := parseFileContent(stub.Content, replacements)
+
+        stub.Name = stubNewName
+        stub.RelativePath = stubNewRelativePath
+        stub.Content = stubNewContent
+
+        parsedStubs = append(parsedStubs, stub)
+    }
+
+    return parsedStubs
+}
+
+// Get all stub files recursively.
 func getStubs(stubsPath string) ([]Stub, error) {
     stubs := []Stub{}
 
     err := filepath.Walk(stubsPath, func(currentPath string, fileInfo os.FileInfo, err error) error {
-        if fileInfo.IsDir() {
+        if err != nil || fileInfo.IsDir() {
             return err
         }
 
@@ -106,34 +142,13 @@ func getStubs(stubsPath string) ([]Stub, error) {
     return stubs, err
 }
 
-func copyFile(source string, destination string, replacements map[string]string) error {
-    sourceFileContent, err := ioutil.ReadFile(source)
-    sourceFileContent = parseFileContent(sourceFileContent, replacements)
-
-    sourceFileContentReader := bytes.NewReader(sourceFileContent)
-
-    destinationFile, err := os.Create(destination)
-    defer destinationFile.Close()
-
-    _, err = io.Copy(destinationFile, sourceFileContentReader)
-
-    return err
-}
-
+// Replace placeholders in file content.
 func parseFileContent(content []byte, replacements map[string]string) []byte {
-    fileContent := string(content)
-
-    fileContent = parseReplacements(fileContent, replacements)
-
-    return []byte(fileContent)
+    return []byte(replacePlaceholders(string(content), replacements))
 }
 
-type Stub struct {
-    FullPath, RelativePath, Name string
-    Content                      []byte
-}
-
-func parseReplacements(input string, replacements map[string]string) string {
+// Replace placeholders with variables.
+func replacePlaceholders(input string, replacements map[string]string) string {
     for key, value := range replacements {
         input = strings.Replace(input, "__"+key+"__", value, -1)
     }
@@ -141,6 +156,7 @@ func parseReplacements(input string, replacements map[string]string) string {
     return input
 }
 
+// Log any errors to console.
 func handleError(err error) {
     if err != nil {
         log.Fatal(err)
